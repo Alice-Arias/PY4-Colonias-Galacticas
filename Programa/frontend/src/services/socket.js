e056d2f26ddeec99a53d4cf6031d3fc1e1062f1d
Alice Arias Salazar <125696332+Alice-Arias@users.noreply.github.com>
2026-06-23 22:08:46 -0600

    Update socket.js

--- Programa/frontend/src/services/socket.js
+++ Programa/frontend/src/services/socket.js
@@ -5,14 +5,8 @@ import { io } from "socket.io-client";
 // Conexión en tiempo real con el backend
 // ======================================================
 
-// Cambia esto según tu entorno (local o ngrok)
 const socket = io(import.meta.env.VITE_SOCKET_URL, {
   transports: ["websocket"]
 });
-
-// Si usas ngrok en el futuro:
-// const socket = io("https://TU-URL-NGROK.ngrok-free.app", {
-//   transports: ["websocket"]
-// });
 
 export default socket;
